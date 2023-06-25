2d50508fbdee2da9e9947f560dd9f54dcdbbfc15
zyzhang <451188360@qq.com>
2023-06-25 22:32:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,9 +1,9 @@
 const BLOG = {
-  title: 'joey的营地',
+  title: 'Yのqq 空间✿✿✿',
   author: 'joeyzh',
   email: '451188360@qq.com',
   link: 'https://joeyzh.vercel.app',
-  description: 'This gonna be an awesome website.',
+  description: '龟龟.',
   lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
